a75ed385e396166c13c426dbc60905d73cd6f3c4
Jingyi Ni <jingyni@amazon.com>
2016-10-10 07:33:50 -0700

    reduce the number of sys.stderr.write call to speed up

--- hw1/parse_input.py
+++ hw1/parse_input.py
@@ -1,7 +1,6 @@
 import csv
-import fileinput
-import sys
 import re
+import sys
 
 
 # csv header
@@ -17,53 +16,59 @@ class CsvField(object):
         self.ebook_id = null
         self.language = null
         self.body_list = []
+        self.token_list = []
 
 
-matchers = [
-    (r'Title: (.*)\b', 'title'),
-    (r'Author: (.*)\b', 'author'),
-    (r'Release Date: (.*) \[EBook #(\d+)\]', ['release_date', 'ebook_id']),
-    (r'Language: (.*)\b', 'language'),
-]
+title_prefix = 'Title: '
+author_prefix = 'Author: '
+release_date_prefix = 'Release Date: '
+language_prefix = 'Language: '
 
 
-def main():
+def process_file(file_path):
     read_body = False
     csv_field = CsvField()
 
     csv.field_size_limit(sys.maxint)
     csv_writer = csv.writer(sys.stdout)
 
-    for line in fileinput.input():
-        if read_body:
-            if line.startswith('*** END OF THE PROJECT GUTENBERG EBOOK'):
-                read_body = False
-                csv_writer.writerow([
-                    csv_field.title,
-                    csv_field.author,
-                    csv_field.release_date,
-                    csv_field.ebook_id,
-                    csv_field.language,
-                    ''.join(csv_field.body_list),
-                ])
-                csv_field = CsvField()
+    with open(file_path, 'r') as f:
+        for line in f:
+            if read_body:
+                if line.startswith('*** END OF THE PROJECT GUTENBERG EBOOK'):
+                    read_body = False
+                    csv_writer.writerow([
+                        csv_field.title,
+                        csv_field.author,
+                        csv_field.release_date,
+                        csv_field.ebook_id,
+                        csv_field.language,
+                        ''.join(csv_field.body_list),
+                    ])
+                    sys.stderr.write(''.join([
+                        '{},{}\r\n'.format(csv_field.ebook_id, token.lower()) for token in csv_field.token_list
+                    ]))
+                    csv_field = CsvField()
+                else:
+                    csv_field.body_list.append(line)
+                    csv_field.token_list.extend(re.findall('[a-zA-Z]+', line))
             else:
-                csv_field.body_list.append(line)
-                for token in re.findall('[a-zA-Z]+', line):
-                    sys.stderr.write("{},{}\r\n".format(csv_field.ebook_id, token.lower()))
-        else:
-            if line.startswith('*** START OF THE PROJECT GUTENBERG EBOOK'):
-                read_body = True
+                if line.startswith('*** START OF THE PROJECT GUTENBERG EBOOK'):
+                    read_body = True
+                elif line.startswith(title_prefix):
+                    csv_field.title = line[len(title_prefix):].strip()
+                elif line.startswith(author_prefix):
+                    csv_field.author = line[len(author_prefix):].strip()
+                elif line.startswith(release_date_prefix):
+                    csv_field.release_date = line[len(release_date_prefix):line.index('[')].strip()
+                    csv_field.ebook_id = line[line.index('#') + 1:line.index(']')]
+                elif line.startswith(language_prefix):
+                    csv_field.language = line[len(language_prefix):].strip()
 
-            for matcher in matchers:
-                match = re.match(matcher[0], line)
-                if match:
-                    fields = matcher[1]
-                    if not isinstance(fields, list):
-                        fields = [fields]
-                    for idx, field in enumerate(fields):
-                        setattr(csv_field, field, match.group(idx + 1))
-                    break
+
+def main():
+    assert len(sys.argv) > 1
+    process_file(sys.argv[1])
 
 
 if __name__ == '__main__':
