821127fb48fcba314f62d9ec8dbab366a45e75f8
Jingyi Ni <jingyni@amazon.com>
2016-10-09 18:43:27 -0700

    python script to parse ebooks.txt

---
+++ hw1/parse_input.py
@@ -0,0 +1,68 @@
+import csv
+import fileinput
+import sys
+import re
+
+
+# csv header
+# title,author,release_date,ebook_id,language,body
+
+
+class CsvField(object):
+    def __init__(self):
+        self.title = None
+        self.author = None
+        self.release_date = None
+        self.ebook_id = None
+        self.language = None
+        self.body_list = []
+
+
+matchers = [
+    (r'Title: (.*)', 'title'),
+    (r'Author: (.*)', 'author'),
+    (r'Release Date: (.*) \[EBook #(\d+)\]', ['release_date', 'ebook_id']),
+    (r'Language: (.*)', 'language'),
+]
+
+
+def main():
+    read_body = False
+    csv_field = CsvField()
+
+    csv.field_size_limit(sys.maxint)
+    csv_writer = csv.writer(sys.stdout)
+
+    for line in fileinput.input():
+        if read_body:
+            if line.startswith('*** END OF THE PROJECT GUTENBERG EBOOK'):
+                read_body = False
+                csv_writer.writerow([
+                    csv_field.title,
+                    csv_field.author,
+                    csv_field.release_date,
+                    csv_field.ebook_id,
+                    csv_field.language,
+                    ''.join(csv_field.body_list)
+                ])
+            else:
+                csv_field.body_list.append(line)
+                for token in re.findall('\w+', line):
+                    sys.stderr.write("{},{}\n".format(csv_field.ebook_id, token.lower()))
+        else:
+            if line.startswith('*** START OF THE PROJECT GUTENBERG EBOOK'):
+                read_body = True
+
+            for matcher in matchers:
+                match = re.match(matcher[0], line)
+                if match:
+                    fields = matcher[1]
+                    if not isinstance(fields, list):
+                        fields = [fields]
+                    for idx, field in enumerate(fields):
+                        setattr(csv_field, field, match.group(idx + 1))
+                    break
+
+
+if __name__ == '__main__':
+    main()
